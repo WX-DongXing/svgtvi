d728c5e8870931fdd3ab99be72fe6494eb361ac5
WX-DongXing <xingdong.com@live.com>
2022-08-02 18:56:10 +0800

    chore: change eslint style

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
-import { readdir } from 'fs/promises';
-import { join, resolve } from 'node:path';
-import { remove, mkdirs } from 'fs-extra';
-import { readFolder } from './utils';
-import { SVGTVCConfig } from './types';
+import { readdir } from 'fs/promises'
+import { join, resolve } from 'node:path'
+import { remove, mkdirs } from 'fs-extra'
+import { readFolder } from './utils'
+import { SVGTVCConfig } from './types'
 
 export default async function svgtvc(options?: SVGTVCConfig) {
   try {
-    const { input, output = 'dist', clear = true } = options ?? {};
+    const { input, output = 'dist', clear = true } = options ?? {}
 
-    const outputPath = join(resolve(), output);
+    const outputPath = join(resolve(), output)
 
-    if (!options?.input) console.error('svgtvc: miss "input" in options');
+    if (!input) {
+      console.error('svgtvc: miss "input" in options')
+      return
+    }
 
-    if (options?.clear) await remove(outputPath);
+    if (options?.clear) await remove(outputPath)
 
-    await mkdirs(outputPath);
+    await mkdirs(outputPath)
 
-    const files = await readFolder(join(resolve(), input!));
+    const files = await readFolder(join(resolve(), input))
 
-    console.log(files);
+    console.log(files)
   } catch (error) {
-    console.error('svgtvc: an error occurred! ', error);
+    console.error('svgtvc: an error occurred! ', error)
   }
 }
 
 svgtvc({
   input: './svgs'
-});
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
-import { readdir } from 'fs/promises';
-import { join } from 'node:path';
-import { SVGFile } from './types';
+import { readdir } from 'fs/promises'
+import { join } from 'node:path'
+import { SVGFile } from './types'
 
 /**
  * get folder svg files
@@ -9,15 +9,15 @@ import { SVGFile } from './types';
  */
 export async function readFolder(path: string): Promise<SVGFile[]> {
   try {
-    const fileNames = await readdir(path);
+    const fileNames = await readdir(path)
     return fileNames
       .filter((fileName: string) => /.svg$/.test(fileName))
       .map((fileName: string) => ({
         name: fileName,
         path: join(path, fileName)
-      }));
+      }))
   } catch (error) {
-    console.error('svgtvc: read folder error! ', error);
-    return [];
+    console.error('svgtvc: read folder error! ', error)
+    return []
   }
 }
